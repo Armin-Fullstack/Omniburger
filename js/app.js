e74905842d646ed18efd96d4c648c3a00299318a
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-05-17 17:16:04 +0330

    feat: implemented mobile menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const btnMenu = document.getElementById("btn-menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 function showMenu() {
-  btnMenu.classList.toggle("open")
+  btnMenu.classList.toggle("open") 
+  mobileMenu.classList.toggle("show-menu")
 }
 btnMenu.addEventListener("click" , showMenu)
