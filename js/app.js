ecb56153742ee7fa4030f8c297e29d07a496a991
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-05-19 17:38:38 +0330

    feat: implemented Scroll functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 const btnMenu = document.getElementById("btn-menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const allLinks = document.querySelectorAll("a:link")
 function showMenu() {
   btnMenu.classList.toggle("open") 
   mobileMenu.classList.toggle("show-menu")
 }
+
+
 btnMenu.addEventListener("click" , showMenu)
+allLinks.forEach(element => {
+  element.addEventListener("click" , function(e) {
+    e.preventDefault()
+    //Reading attribute
+    const href = element.getAttribute("href")
+    if(href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    } else if(href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href)
+      // we did not use scrollTo bc we don't know the pixel from the top to scroll
+      sectionEl.scrollIntoView({
+        behavior: "smooth"
+      })
+      if(element.classList.contains("main-nav-link")) {
+        btnMenu.classList.toggle("open") 
+        mobileMenu.classList.toggle("show-menu")
+      }
+    }
+    
+  })
+})
